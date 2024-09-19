5a084bea310304011b5f678530b5276655c3f071
jacosta56 <acostaquinterojuanfelipe@gmail.com>
2024-09-19 11:35:33 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import miLogo from "../src/assets/img/imagen_principal.png"
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <img src={miLogo} alt='Logo de React' />
+        <h1> El Cerezo Sagrado </h1>
+        <p>Hogar del Trueno</p>
+      </div>
+    </>
+  )
+}
+
+export default App
